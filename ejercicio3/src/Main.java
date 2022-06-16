721642062d7fb69b2a6795eff7369c724fd27265
SuperJav <javielito2405@hotmail.com>
2022-06-15 21:00:46 -0400

    subida del ejercico3

---
+++ ejercicio3/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Persona persona1 =new Persona();
+        persona1.setEdad(23);
+        persona1.setNombre("Javier");
+        persona1.setTelefono("890-098-098");
+        System.out.println(persona1.getEdad());
+        System.out.println(persona1.getNombre());
+        System.out.println(persona1.getTelefono());
+
+    }
+   static class Persona{
+        private int edad;
+        private String nombre;
+        private String telefono;
+
+        public void setEdad(int edad){
+            this.edad=edad;
+        }
+        public int getEdad(){
+            return this.edad;
+        }
+
+        public void setNombre(String nombre){
+            this.nombre=nombre;
+        }
+        public String getNombre(){
+            return this.nombre;
+        }
+
+        public void setTelefono(String telefono){
+            this.telefono=telefono;
+        }
+        public String getTelefono(){
+            return this.telefono;
+        }
+    }
+}
